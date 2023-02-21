a40fbf08e491dab80d63f3d6408ac491bf836293
indranil Bakshi <iamneel67@gmail.com>
2023-02-21 20:11:34 +0530

    neel changes

--- main.go
+++ main.go
@@ -1,27 +1,47 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
 	"text/template"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
-var templ *template.Template
+var tmpl *template.Template
+var db *sql.DB
 
 type studentInfo struct {
 	Name   string
 	course string
 }
 
+func getSql() *sql.DB {
+
+	db, err := sql.Open("mysql", "root:@(127.0.0.1:3306)/go?parseTime=true")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("connection established")
+	return db
+
+}
+
 func init() {
 
-	templ = template.Must(template.ParseFiles("crud.html"))
+	tmpl = template.Must(template.ParseFiles("crud.html"))
 }
 
 func crudHandler(w http.ResponseWriter, r *http.Request) {
 
+	db = getSql()
+
 	if r.Method != http.MethodPost {
-		templ.Execute(w, nil)
+		tmpl.Execute(w, nil)
 		return
 	}
 
@@ -31,7 +51,37 @@ func crudHandler(w http.ResponseWriter, r *http.Request) {
 		course: r.FormValue("course"),
 	}
 
-	fmt.Println(Student)
+	//insert student
+
+	if r.FormValue("submit") == "insert" {
+
+		_, err := db.Exec("insert into register(name,course) values(?,?)", Student.Name, Student.course)
+
+		if err != nil {
+
+			log.Fatal(err)
+			tmpl.Execute(w, struct {
+				success bool
+				message string
+			}{success: true, message: err.Error()})
+		} else {
+
+			tmpl.Execute(w, struct {
+				success bool
+				message string
+			}{success: true, message: "Record inserted"})
+
+		}
+
+	} else if r.FormValue("submit") == "read" {
+
+		tmpl.Execute(w, struct {
+			success bool
+			message string
+		}{success: true, message: "welcome to user list"})
+
+	}
+
 }
 
 func main() {
